ef321e6353f1b40f0007d38465d19b7e9a55d993
Alex <ecommerce-technician@users.noreply.github.com>
2016-12-09 12:21:34 -0700

    Update create-ionic-app.sh

--- create-ionic-app.sh
+++ create-ionic-app.sh
@@ -5,7 +5,8 @@ ionic platform add android
 ionic build android
 ionic emulate
 ionic serve
-
+#Set autoreload memory limit higher
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 
     
 
